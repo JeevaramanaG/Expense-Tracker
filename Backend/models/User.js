c483c18e85bc7aff179c39cf8ff71eac5b097441
JeevaramanaG <chitramurugesank@gmail.com>
2024-05-08 23:21:59 +0530

    userSchema

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,1 +1,28 @@
+const { default: mongoose } = require("mongoose");
+const mongodb = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps:true
+  }
+);
+
+module.exports =mongoose.model("User",userSchema)
 
