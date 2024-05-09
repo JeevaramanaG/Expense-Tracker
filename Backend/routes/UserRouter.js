a331e8b1c8a0719c3cc551c4ec260d29e8e7cec1
JeevaramanaG <chitramurugesank@gmail.com>
2024-05-09 20:06:58 +0530

    created api for login

--- Backend/routes/UserRouter.js
+++ Backend/routes/UserRouter.js
@@ -3,5 +3,6 @@ const userController = require("../controllers/UserCtrl");
 const userRouter = express.Router();
 
 userRouter.post("/api/v1/users/register", userController.register);
+userRouter.get("/api/v1/users/login", userController.login);
 
 module.exports = userRouter;
