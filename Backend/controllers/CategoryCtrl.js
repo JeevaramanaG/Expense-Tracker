b97c51661b3ae1e3a2fcae1447d533cd650fa062
JeevaramanaG <chitramurugesank@gmail.com>
2024-05-10 19:40:43 +0530

    created category controller

---
+++ Backend/controllers/CategoryCtrl.js
@@ -0,0 +1,12 @@
+const asyncHandler = require("express-async-handler");
+const Category = require("../models/Category");
+
+const CategoryController = {
+  create: asyncHandler(async (req, res) => {}),
+
+  lists: asyncHandler(async (req, res) => {}),
+
+  update: asyncHandler(async (req, res) => {}),
+
+  delete: asyncHandler(async (req, res) => {}),
+};
