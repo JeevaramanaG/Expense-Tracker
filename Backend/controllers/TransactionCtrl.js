c1ca727a7b34c5d0631ed881679e803398236579
JeevaramanaG <chitramurugesank@gmail.com>
2024-05-11 11:10:04 +0530

    update and delete method

--- Backend/controllers/TransactionCtrl.js
+++ Backend/controllers/TransactionCtrl.js
@@ -24,6 +24,64 @@ const userTransaction = {
     const transactions = await Transaction.find({ user: req.user });
     res.json(transactions);
   }),
+
+  getFilterTransaction: asyncHandler(async (req, res) => {
+    const { type, startDate, endDate, category } = req.query;
+    let filter = { user: req.user };
+
+    // Filtered by StartDate
+    if (startDate) {
+      filter.date = { ...filter.date, $gte: new Date(startDate) };
+    }
+    // Filtered by EndDate
+    if (endDate) {
+      filter.date = { ...filter.date, $lte: new Date(endDate) };
+    }
+    // Filtered by Type
+    if (type) {
+      filter.type = type;
+    }
+    if (category) {
+      if (category === "All") {
+        // no filter needed
+      } else if (category === "Uncategorized") {
+        filter.category = "Uncategorized";
+      } else {
+        filter.category = category;
+      }
+    }
+    const transaction = await Transaction.find(filter).sort({ date: -1 });
+    res.json(transaction);
+  }),
+
+  //Update Transaction
+  updateTransaction: asyncHandler(async (req, res) => {
+    const transaction = await Transaction.findById(req.params.id);
+    if (transaction && transaction.user.toString() === req.user.toString()) {
+      // Update transaction properties if they are provided in the request body
+      if (req.body.category) transaction.category = req.body.category;
+      if (req.body.type) transaction.type = req.body.type;
+      if (req.body.amount) transaction.amount = req.body.amount;
+      if (req.body.date) transaction.date = req.body.date;
+      if (req.body.description) transaction.description = req.body.description;
+
+      // Save the updated transaction
+      const updatedTransaction = await transaction.save();
+      // Send the updated transaction as the response
+      res.json(updatedTransaction);
+    } else {
+      // Return a 404 Not Found response
+      res.status(404).json({ error: "Transaction not found or unauthorized" });
+    }
+  }),
+  // delete Transaction
+  deleteTransaction: asyncHandler(async (req, res) => {
+    const transaction = await Transaction.findById(req.params.id);
+    if (transaction && transaction.user.toString() === req.user.toString()) {
+      await Transaction.findByIdAndDelete(req.params.id);
+      res.json({ message: "Transaction removed" });
+    }
+  }),
 };
 
 module.exports = userTransaction;
--- Backend/routes/TransactionRouter.js
+++ Backend/routes/TransactionRouter.js
@@ -3,16 +3,41 @@ const transactionRouter = express.Router();
 const transactionController = require("../controllers/TransactionCtrl");
 const isAuthenticated = require("../middlewares/isAuthentication");
 
+//Tansaction API for add
 transactionRouter.post(
   "/api/v1/transaction/add",
   isAuthenticated,
   transactionController.add
 );
 
+//Transaction API for lists the data
 transactionRouter.get(
   "/api/v1/transaction/lists",
   isAuthenticated,
   transactionController.lists
+);
+
+// API for filtering
+transactionRouter.get(
+  "/api/v1/transaction/filter",
+  isAuthenticated,
+  transactionController.getFilterTransaction
+);
+
+// Api for Update Transaction
+
+transactionRouter.put(
+  "/api/v1/transaction/update/:id",
+  isAuthenticated,
+  transactionController.updateTransaction
+);
+
+// API for Deleting Transaction
+
+transactionRouter.delete(
+  "/api/v1/transaction/delete/:id",
+  isAuthenticated,
+  transactionController.deleteTransaction
 );
 
 module.exports = transactionRouter;
